cc39e9e0dc29e9317aa3b11946c3e403d3d43c67
Chanchal Dhingra <chanchaldhingra.17@gmail.com>
2024-08-18 08:42:50 +0530

    ep5 using named import/export

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,16 +1,15 @@
+import {IMG_URL} from "../utils/constants";
 
-const TileComp = (props) => {
+export const TileComp = (props) => {
     const {resData} = props;
     const {tileName, rating} = resData;
     return (
 
         <div className='tile-container'>
-            <img src='https://www.pixelstalk.net/wp-content/uploads/2016/08/Fresh-hot-delicious-food-wallpaper.jpg' alt='food-image'
+            <img src={IMG_URL} alt='food-image'
                 className='tile-image' />
             <h2 className="tile-name">{tileName}</h2>
             <span className='tile-rating'>Rating - {rating} / 5</span>
         </div>
     )
 };
-
-export default TileComp;
