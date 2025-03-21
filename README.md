d4a05b033bda5ce0fb4d0db9a8698f1f19973bf7
Chanchal Dhingra <cdhingra@networkrail.co.uk>
2025-03-22 00:10:55 +0530

    api call lect 6

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # namaste-react
+
+useState Hook:-
+
+whenever value of useState variable changes, react re-render the component.
+
+Syntax-
+
+const [listOfRestaurant, setListOfRestaurant] = useState([]);  
+
+// value inside the useState() is default value
+
+to update value of listOfRestaurants use setListOfRestaurant()
+
+ex- setListOfRestaurant([]);
+
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,36 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
+
+    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
+    useEffect(()=> {
+        console.log("useEffect called");
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.298655195278855&lng=78.01370646804571&collection=83631&tags=layout_CCS_Pizza&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+        const json = await data.json();
+        console.log("data ", json);
+    };
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = listOfRestaurant.filter(ele => ele.avgRating>4);
+                    setListOfRestaurant(filteredList);
+                }}>
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-container">
                 {
-                    resList.map(restaurant => <RestaurantCard key={restaurant.id} resName = {restaurant} />)
+                    listOfRestaurant.map(restaurant => <RestaurantCard key={restaurant.id} resName = {restaurant} />)
                 }
             </div>
         </div>
